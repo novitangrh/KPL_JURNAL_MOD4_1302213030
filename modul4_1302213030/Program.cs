dbc1f052c05ae3dbafe6a19772629bc6ba6a7550
PC16-IFLAB <novitasabila70@gmail.com>
2023-03-13 07:30:58 +0700

    menambahkan class KodeBuah

--- modul4_1302213030/Program.cs
+++ modul4_1302213030/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Test");
+using System.Diagnostics.Contracts;
+
+internal class KodeBuah
+{
+    private string[] buah = { "Apel", "Aprikot", "Alpukat", "Pisang", "Paprika", "Blackberry", 
+        "Ceri", "Kelapa", "Jagung","Kurma", "Durian", "Anggur", "Melon", "Semangka"};
+    private string[] kode = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", 
+        "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+
+    public string getKodeBuah(string fruit)
+    {
+        for(int i = 0; i < buah.Length; i++)
+        {
+            if (buah[i] == fruit)
+            {
+                return "Kode buah: " + kode[i];
+            }
+        }
+        return "Buah tidak tesedia";
+    }
+}
+
+class Program
+{
+    public static void Main(string[] agrs)
+    {
+        KodeBuah kode = new KodeBuah();
+        Console.Write("Nama buah: ");
+        string buah = Console.ReadLine();
+        Console.WriteLine(kode.getKodeBuah(buah));
+    }
+}
